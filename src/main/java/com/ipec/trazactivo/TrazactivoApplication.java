5fe47910e2a73b849f2ab14a3d4fdd11957d102f
jmosro <jmosro.14@gmail.com>
2021-11-25 13:37:41 -0600

    Spring boot initialzr project.

---
+++ src/main/java/com/ipec/trazactivo/TrazactivoApplication.java
@@ -0,0 +1,13 @@
+package com.ipec.trazactivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrazactivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrazactivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipec/trazactivo/TrazactivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipec.trazactivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrazactivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
