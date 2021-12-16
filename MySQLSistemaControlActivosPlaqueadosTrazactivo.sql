f9e4317a441aec077ea407e9beb1506b148ad493
Jesús Alberto Moscoso Agüero <jmosro@192.168.32.4>
2021-12-16 11:54:42 -0600

    Actualización de los datos en la base de datos.

--- MySQLSistemaControlActivosPlaqueadosTrazactivo.sql
+++ MySQLSistemaControlActivosPlaqueadosTrazactivo.sql
@@ -143,172 +143,178 @@ Insertar datos a las tablas
 -- Fuente: Directriz DVM-PICR-235-2014, San José, 12 de marzo, 2014. MEP.
 -- Mobiliario y equipo de oficina
 insert into trazactivo.descripcion_activo values 
-(default, "archivador"), (default, "gabinete"), (default, "biblioteca"),
-(default, "escritorio para oficina"), (default, "armario para oficina"), (default, "mesa para oficina"),
-(default, "silla para oficina"), (default, "banco para oficina"),
-(default, "mueble de varios tipos y uso en oficina"), (default, "mobiliario para computadora de oficina"),
-(default, "juego de comedor para oficina"), (default, "juego de sala para oficina"),
-(default, "estantería"), (default, "pieza y obra de colección"),
-(default, "pintura"), (default, "escultura"), (default, "enciclopedia"),
-(default, "video educacional"), (default, "máquina destructora de documentos"),
-(default, "trituradora de documentos"),
-(default, "máquina encuadernadora"), (default, "empastadora de litografía y poligrafía"),
-(default, "artefacto eléctrico de oficina"), (default, "artefacto electrónico de oficina"),
-(default, "calculadora"), (default, "sumadora"),
-(default, "fotocopiadora"), (default, "máquina de escribir"),
-(default, "abanico"), (default, "ventilador"), (default, "aire acondicionado"),
-(default, "lámpara"), (default, "reloj"), (default, "reloj marcador"),
-(default, "tarjetero");
+(default, "No indica"),
+(default, "Archivador"), (default, "Gabinete"), (default, "Biblioteca"),
+(default, "Escritorio para oficina"), (default, "Armario para oficina"), (default, "Mesa para oficina"),
+(default, "Silla para oficina"), (default, "Banco para oficina"),
+(default, "Mueble de varios tipos y uso en oficina"), (default, "Mobiliario para computadora de oficina"),
+(default, "Juego de comedor para oficina"), (default, "Juego de sala para oficina"),
+(default, "Estantería"), (default, "Pieza y obra de colección"),
+(default, "Pintura"), (default, "Escultura"), (default, "Enciclopedia"),
+(default, "Video educacional"), (default, "Máquina destructora de documentos"),
+(default, "Trituradora de documentos"),
+(default, "Máquina encuadernadora"), (default, "Empastadora de litografía y poligrafía"),
+(default, "Artefacto eléctrico de oficina"), (default, "Artefacto electrónico de oficina"),
+(default, "Calculadora"), (default, "Sumadora"),
+(default, "Fotocopiadora"), (default, "Máquina de escribir"),
+(default, "Abanico"), (default, "Ventilador"), (default, "Aire acondicionado"),
+(default, "Lámpara"), (default, "Reloj"), (default, "Reloj marcador"),
+(default, "Tarjetero");
 -- Equipo de cómputo
 insert into trazactivo.descripcion_activo values 
-(default, "cpu"), (default, "monitor"), (default, "teclado"),
-(default, "mouse o ratón"), (default, "laptop"),
-(default, "notebook"), (default, "servidor"), (default, "impresora"),
-(default, "licencia para software"), (default, "disco duro interno"), (default, "disco duro externo"),
-(default, "módem"), (default, "cámara web"), (default, "scanner"),
-(default, "unidad lectora"), (default, "quemador de discos interno"), (default, "quemador de discos externo"),
-(default, "micrófono para multimedia"), (default, "parlantes para multimedia"),
-(default, "auricular para multimedia"), (default, "headset para multimedia"),
-(default, "llave maya");
+(default, "CPU"), (default, "Monitor"), (default, "Teclado"),
+(default, "Mouse o ratón"), (default, "Laptop"),
+(default, "Notebook"), (default, "Servidor"), (default, "Impresora"),
+(default, "Licencia para software"), (default, "Disco duro interno"), (default, "Disco duro externo"),
+(default, "Módem"), (default, "Cámara web"), (default, "Scanner"),
+(default, "Unidad lectora"), (default, "Quemador de discos interno"), (default, "Quemador de discos externo"),
+(default, "Micrófono para multimedia"), (default, "Parlantes para multimedia"),
+(default, "Auricular para multimedia"), (default, "Headset para multimedia"),
+(default, "Llave maya");
 -- Equipo electrónico
 insert into trazactivo.descripcion_activo values 
-(default, "equipo de grabación audio y video"), (default, "cámara para fotografía"),
-(default, "cámara de video"),
-(default, "cámara de seguridad"), (default, "televisor"), (default, "pantalla"),
-(default, "dvd"), (default, "vhs"), (default, "otros proyectores de video"),
-(default, "pantalla reflectora"), (default, "video beam"), (default, "proyector"),
-(default, "equipo de sonido"), (default, "grabadora"), (default, "radio"),
-(default, "amplificador"), (default, "planta electrónica"), (default, "pedestal"),
-(default, "audífono"), (default, "megáfono"), (default, "grabadora tipo periodista"),
-(default, "tarjeta de memoria para cámara digital"),
-(default, "router"), (default, "hub o concentrador"), (default, "switch"),
-(default, "equipo de proyección y videoconferencia"), (default, "equipo de comunicación"),
-(default, "antena"),
-(default, "central telefónica"), (default, "teléfono"), (default, "fax"),
-(default, "equipo de radiocomunicación"), (default, "radar");
+(default, "Equipo de grabación audio y video"), (default, "Cámara para fotografía"),
+(default, "Cámara de video"),
+(default, "Cámara de seguridad"), (default, "Televisor"), (default, "Pantalla"),
+(default, "DVD"), (default, "VHS"), (default, "Otros proyectores de video"),
+(default, "Pantalla reflectora"), (default, "Video beam"), (default, "Proyector"),
+(default, "Equipo de sonido"), (default, "Grabadora"), (default, "Radio"),
+(default, "Amplificador"), (default, "Planta electrónica"), (default, "Pedestal"),
+(default, "Audífono"), (default, "Megáfono"), (default, "Grabadora tipo periodista"),
+(default, "Tarjeta de memoria para cámara digital"),
+(default, "Router"), (default, "Hub o concentrador"), (default, "Switch"),
+(default, "Equipo de proyección y videoconferencia"), (default, "Equipo de comunicación"),
+(default, "Antena"),
+(default, "Central telefónica"), (default, "Teléfono"), (default, "Fax"),
+(default, "Equipo de radiocomunicación"), (default, "Radar");
 -- Herramientas eléctricas y manuales
 insert into trazactivo.descripcion_activo values 
-(default, "equipo solar colector"), (default, "equipo solar panel"), (default, "equipo solar rastreador"),
-(default, "equipo solar planta eléctrica"), (default, "equipo para soldar"), (default, "equipo de bombeo"),
-(default, "generador de electricidad"), (default, "equipo para pintar"), (default, "herramienta de mecánica automotriz"),
-(default, "equipo de mecánica automotriz"), (default, "equipo de medición"), (default, "motor"),
-(default, "equipo hidráulico"), (default, "cortadora"), (default, "máquina de coser");
+(default, "Equipo solar colector"), (default, "Equipo solar panel"), (default, "Equipo solar rastreador"),
+(default, "Equipo solar planta eléctrica"), (default, "Equipo para soldar"), (default, "Equipo de bombeo"),
+(default, "Generador de electricidad"), (default, "Equipo para pintar"), (default, "Herramienta de mecánica automotriz"),
+(default, "Equipo de mecánica automotriz"), (default, "Equipo de medición"), (default, "Motor"),
+(default, "Equipo hidráulico"), (default, "Cortadora"), (default, "Máquina de coser");
 -- Equipo de aula
 insert into trazactivo.descripcion_activo values 
-(default, "pizarra"), (default, "pupitre"),
-(default, "escritorio para colegio"),
-(default, "armario para colegio"), (default, "mesa para colegio"),
-(default, "silla para colegio"), (default, "banco para colegio");
+(default, "Pizarra"), (default, "Pupitre"),
+(default, "Escritorio para colegio"),
+(default, "Armario para colegio"), (default, "Mesa para colegio"),
+(default, "Silla para colegio"), (default, "Banco para colegio");
 -- Otros equipos
 insert into trazactivo.descripcion_activo values 
-(default, "equipo sanitario"), (default, "equipo de laboratorio"), (default, "equipo de investigación"),
-(default, "equipo meteorológico"), (default, "equipo médico"), (default, "mobiliario médico"),
-(default, "equipo de rescate"), (default, "equipo de primeros auxilios"), (default, "equipo para animal"),
-(default, "equipo veterinario");
+(default, "Equipo sanitario"), (default, "Equipo de laboratorio"), (default, "Equipo de investigación"),
+(default, "Equipo meteorológico"), (default, "Equipo médico"), (default, "Mobiliario médico"),
+(default, "Equipo de rescate"), (default, "Equipo de primeros auxilios"), (default, "Equipo para animal"),
+(default, "Equipo veterinario");
 -- Vehículos
 insert into trazactivo.descripcion_activo values 
-(default, "vehículo liviano"), (default, "vehículo de carga"), (default, "bus"),
-(default, "microbús"), (default, "vehículo para transporte"),
-(default, "vehículo para levantamiento de carga"),
-(default, "motocicleta"), (default, "cuadriciclo"), (default, "bicicleta"),
-(default, "lancha"), (default, "bote");
+(default, "Vehículo liviano"), (default, "Vehículo de carga"), (default, "Bus"),
+(default, "Microbús"), (default, "Vehículo para transporte"),
+(default, "Vehículo para levantamiento de carga"),
+(default, "Motocicleta"), (default, "Cuadriciclo"), (default, "Bicicleta"),
+(default, "Lancha"), (default, "Bote");
 -- Instrumentos musicales
 insert into trazactivo.descripcion_activo values 
-(default, "guitarra"), (default, "flauta"), (default, "tambor");
+(default, "Guitarra"), (default, "Flauta"), (default, "Tambor");
 -- Deportivo
 insert into trazactivo.descripcion_activo values 
-(default, "guante para baseball"), (default, "pelota"), (default, "rodillera");
+(default, "Guante para baseball"), (default, "Pelota"), (default, "Rodillera");
 -- Cocina
 insert into trazactivo.descripcion_activo values 
-(default, "olla"), (default, "sartén"), (default, "percolador"),
-(default, "coffee maker"), (default, "cocina eléctrica"), (default, "cocina de gas"),
-(default, "refrigeradora"), (default, "congelador"), (default, "enfriador"),
-(default, "plantilla eléctrica"), (default, "plantilla de gas"), (default, "extintor"),
-(default, "horno eléctrico"), (default, "microondas"), (default, "freidor"),
-(default, "tanque de agua"), (default, "mueble para fregadero"), (default, "balanza"),
-(default, "báscula"), (default, "romana"), (default, "extractor");
+(default, "Olla"), (default, "Sartén"), (default, "Percolador"),
+(default, "Coffee maker"), (default, "Cocina eléctrica"), (default, "Cocina de gas"),
+(default, "Refrigeradora"), (default, "Congelador"), (default, "Enfriador"),
+(default, "Plantilla eléctrica"), (default, "Plantilla de gas"), (default, "Extintor"),
+(default, "Horno eléctrico"), (default, "Microondas"), (default, "Freidor"),
+(default, "Tanque de agua"), (default, "Mueble para fregadero"), (default, "Balanza"),
+(default, "Báscula"), (default, "Romana"), (default, "Extractor");
 -- Implementos y equipo duradero de limpieza, seguridad y mantenimiento
 insert into trazactivo.descripcion_activo values 
-(default, "máquina para lavado a presión"), (default, "aspiradora"), (default, "lavadora"),
-(default, "trapeador"), (default, "cepillo eléctrico"), (default, "secamanos"),
-(default, "batería sanitaria"), (default, "inodoro"), (default, "urinal"),
-(default, "lavamanos"), (default, "cortadora de zacate"), (default, "podadora de zacate"),
-(default, "motoguadaña"), (default, "motosierra"), (default, "arma de fuego"),
-(default, "caseta de seguridad"), (default, "alarma"), (default, "sirena"),
-(default, "sensor"), (default, "reflector"), (default, "lámpara de emergencia");
+(default, "Máquina para lavado a presión"), (default, "Aspiradora"), (default, "Lavadora"),
+(default, "Trapeador"), (default, "Cepillo eléctrico"), (default, "Secamanos"),
+(default, "Batería sanitaria"), (default, "Inodoro"), (default, "Urinal"),
+(default, "Lavamanos"), (default, "Cortadora de zacate"), (default, "Podadora de zacate"),
+(default, "Motoguadaña"), (default, "Motosierra"), (default, "Arma de fuego"),
+(default, "Caseta de seguridad"), (default, "Alarma"), (default, "Sirena"),
+(default, "Sensor"), (default, "Reflector"), (default, "Lámpara de emergencia");
 
 -- Tabla estado
 insert into trazactivo.estado_activo values
-(default, "excelente"),
-(default, "bueno"),
-(default, "regular"),
-(default, "malo");
+(default, "No indica"),
+(default, "Excelente"),
+(default, "Bueno"),
+(default, "Regular"),
+(default, "Malo");
 
 -- Tabla aula
 insert into trazactivo.numero_aula values
-(default, "laboratorio 5"),
-(default, "laboratorio 6"),
-(default, "laboratorio 15"),
-(default, "laboratorio portátil 1"),
-(default, "laboratorio portátil 2"),
-(default, "aula 01"), (default, "aula 02"), (default, "aula 03"),
-(default, "aula 04"), (default, "aula 05"), (default, "aula 06"),
-(default, "aula 07"), (default, "aula 08"), (default, "aula 09"),
-(default, "aula 10"), (default, "aula 11"), (default, "aula 12"),
-(default, "aula 13"), (default, "aula 14"), (default, "aula 15"),
-(default, "aula 16"), (default, "aula 17"), (default, "aula 18"),
-(default, "aula 19"), (default, "aula 20"), (default, "aula 21"),
-(default, "aula 22"), (default, "aula 23"), (default, "aula 24"),
-(default, "aula 25"), (default, "aula 26"), (default, "aula 27"),
-(default, "aula 28"),
-(default, "biblioteca"),
-(default, "dirección"),
-(default, "coordinación técnica"),
-(default, "coordinación con la empresa"),
-(default, "orientación"),
-(default, "auxiliar administrativo"),
-(default, "asistente de dirección");
+(default, "No indica"),
+(default, "Laboratorio 5"),
+(default, "Laboratorio 6"),
+(default, "Laboratorio 15"),
+(default, "Laboratorio portátil 1"),
+(default, "Laboratorio portátil 2"),
+(default, "Aula 01"), (default, "Aula 02"), (default, "Aula 03"),
+(default, "Aula 04"), (default, "Aula 05"), (default, "Aula 06"),
+(default, "Aula 07"), (default, "Aula 08"), (default, "Aula 09"),
+(default, "Aula 10"), (default, "Aula 11"), (default, "Aula 12"),
+(default, "Aula 13"), (default, "Aula 14"), (default, "Aula 15"),
+(default, "Aula 16"), (default, "Aula 17"), (default, "Aula 18"),
+(default, "Aula 19"), (default, "Aula 20"), (default, "Aula 21"),
+(default, "Aula 22"), (default, "Aula 23"), (default, "Aula 24"),
+(default, "Aula 25"), (default, "Aula 26"), (default, "Aula 27"),
+(default, "Aula 28"),
+(default, "Biblioteca"),
+(default, "Dirección"),
+(default, "Coordinación técnica"),
+(default, "Coordinación con la empresa"),
+(default, "Orientación"),
+(default, "Auxiliar administrativo"),
+(default, "Asistente de dirección");
 
 -- Tabla especialidad
 insert into trazactivo.especialidad_academica values
-(default, "técnica"),
-(default, "curso libre"),
-(default, "plan de estudios"),
-(default, "administrativo");
+(default, "No indica"),
+(default, "Técnica"),
+(default, "Curso libre"),
+(default, "Plan de estudios"),
+(default, "Administrativo");
 
 -- Tabla adquisicion
 insert into trazactivo.modo_adquisicion values
-(default, "por junta administrativa"),
-(default, "por recursos del mep"),
-(default, "por recursos del mep ley 7372"),
-(default, "por donación persona física"),
-(default, "por donación persona jurídica");
+(default, "No indica"),
+(default, "Por junta administrativa"),
+(default, "Por recursos del MEP"),
+(default, "Por recursos del MEP ley 7372"),
+(default, "Por donación persona física"),
+(default, "Por donación persona jurídica");
 
 -- Tabla tipo_anotacion
 insert into trazactivo.tipo_anotacion values
-(default, "actualización de estado"),
-(default, "robo o hurto"),
-(default, "cambio de ubicación"),
-(default, "baja de bienes"),
-(default, "verificación aleatoria de inventario"),
-(default, "reparación");
+(default, "No indica"),
+(default, "Actualización de estado"),
+(default, "Robo o hurto"),
+(default, "Cambio de ubicación"),
+(default, "Baja de bienes"),
+(default, "Verificación aleatoria de inventario"),
+(default, "Reparación");
 
+-- Tabla responsable
+insert into trazactivo.persona_responsable values
+(default, "No indica"),
+(default, "profesor 1"),
+(default, "profesor 2"),
+(default, "profesor 3");
 
 /******************************************
 Insertar datos a la tabla activo - Ejemplos
 ******************************************/
 
--- Tabla responsable
-insert into trazactivo.persona_responsable values
-(default, "profesor 1"),
-(default, "profesor 2"),
-(default, "profesor 3");
-
 insert into trazactivo.activo values
-(default, 4864, 40, default, default, default, 1, 10, 40, 1, 0,  2, 1, 1, 1),
-(default, 4864, 36, default, default, default, 1, 10, 41, 2, 0,  5, 2, 2, 3),
-(default, 4864, 109, default, default, default, 1, 12, 42, 4, 1,  3, 3, 1, 3),
-(default, 4864, 153, default, default, default, 1, 12, 43, 1, 0,  7, 2, 1, 1);
+(default, 4864, 41, default, default, default, 1, 10, 40, 1, 0, 2, 1, 4, 1),
+(default, 4864, 37, default, default, default, 1, 10, 41, 2, 0, 5, 2, 2, 3),
+(default, 4864, 110, default, default, default, 1, 12, 42, 4, 1, 3, 3, 3, 3),
+(default, 4864, 154, default, default, default, 1, 12, 43, 1, 0, 7, 2, 1, 1);
 
 -- Tabla activo_observacion
 insert into trazactivo.activo_observacion values
